061c0460e0c182cea0f65355f2c655cb4f56167b
Cleber Del Rei Mendes Rosa Neto <cleberdelrei@gmail.com>
2020-03-10 14:51:05 -0300

    Bombs and Coins logic

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 //SERVER
 const server = require('http').createServer(app).listen(3000)
- 
+
 const io = require('socket.io')(server, {
 	pingInterval: 15000,
 	pingTimeout: 30000,
@@ -34,12 +34,12 @@ app.use('/', router);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser()); 
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //ERROR HANDLER
 app.use(function(req, res, next) {
-  	next(createError(404));
+		next(createError(404));
 });
 app.use(function(err, req, res, next) {
 	res.locals.message = err.message;
@@ -49,36 +49,39 @@ app.use(function(err, req, res, next) {
 //GAME
 
 const coinGenerator = setInterval(function() {
+	let date = new Date();
 	var x = Math.floor(Math.random() * 26)
 	var y = Math.floor(Math.random() * 26)
 	var color = 'rgb(216, 174, 57)'
-	game.coins.push({x, y, color})
+	game.coins.push({id: date.getTime(), x: x, y: y, color: color})
 }, 5000);
 
 const bombGenerator = setInterval(function() {
+	let date = new Date();
 	var x = Math.floor(Math.random() * 26)
 	var y = Math.floor(Math.random() * 26)
 	var color = 'black';
-	game.bombs.push({x, y, color})
+	game.bombs.push({id: date.getTime(), x: x, y: y, color: color})
 }, 25000);
 
 var game = {
-    players: [
+	players: [
 
-    ],
-    coins: [
+	],
+	coins: [
 
 	],
 	bombs: [
 
-    ]
+	]
 };
 
 //IO
 io.on('connection', socket => {
-	socket.emit('connection');
+	socket.emit('connection', socket.id);
 
 	socket.on('playerConnected', player => {
+		player.id = socket.id;
 		game.players.push(player);
 	});
 
@@ -86,6 +89,135 @@ io.on('connection', socket => {
 		socket.emit('renderGame', game);
 		socket.broadcast.emit('renderGame', game);
 	});
+
+	socket.on('keyPress', key => {
+		keyHandler = {
+			'ArrowUp': function (game) {
+				game.players.map((player) => {
+					if(player.id == socket.id  && player.y > 0) {
+						player.y--;
+
+						for(let [index, coin] of game.coins.entries()) {
+							if(player.x == coin.x && player.y == coin.y) {
+								game.coins.splice(index, 1);
+								player.points++;
+							}
+						}
+						for(let [index, enemyPlayer] of game.players.entries()) {
+							if(player.x == enemyPlayer.x && player.y == enemyPlayer.y && player.id != enemyPlayer.id) {
+								game.players.splice(index, 1);
+								player.points = player.points + 10;
+							}
+						}
+						for(let [index, bomb] of game.bombs.entries()) {
+							if(player.x == bomb.x && player.y == bomb.y) {
+								game.bombs.splice(index, 1);
+								player.points = player.points - 10;
+								game.players = game.players.filter((p) => {
+									return p.id != player.id;
+								}, player);
+							}
+						}
+					}
+				})
+			},
+			'ArrowDown': function(game) {
+				game.players.map((player) => {
+					if(player.id == socket.id  && player.y < 24) {
+						player.y++;
+
+						for(let [index, coin] of game.coins.entries()) {
+							if(player.x == coin.x && player.y == coin.y) {
+								game.coins.splice(index, 1);
+								player.points++;
+							}
+						}
+						for(let [index, enemyPlayer] of game.players.entries()) {
+							if(player.x == enemyPlayer.x && player.y == enemyPlayer.y && player.id != enemyPlayer.id) {
+								game.players.splice(index, 1);
+								player.points = player.points + 10;
+							}
+						}
+						for(let [index, bomb] of game.bombs.entries()) {
+							if(player.x == bomb.x && player.y == bomb.y) {
+								game.bombs.splice(index, 1);
+								player.points = player.points - 10;
+								game.players = game.players.filter((p) => {
+									return p.id != player.id;
+								}, player);
+							}
+						}
+					}
+				})
+			},
+			'ArrowRight': function(game) {
+				game.players.map((player) => {
+					if(player.id == socket.id && player.x < 24) {
+						player.x++;
+
+						for(let [index, coin] of game.coins.entries()) {
+							if(player.x == coin.x && player.y == coin.y) {
+								game.coins.splice(index, 1);
+								player.points++;
+							}
+						}
+						for(let [index, enemyPlayer] of game.players.entries()) {
+							if(player.x == enemyPlayer.x && player.y == enemyPlayer.y && player.id != enemyPlayer.id) {
+								game.players.splice(index, 1);
+								player.points = player.points + 10;
+							}
+						}
+						for(let [index, bomb] of game.bombs.entries()) {
+							if(player.x == bomb.x && player.y == bomb.y) {
+								game.bombs.splice(index, 1);
+								player.points = player.points - 10;
+								game.players = game.players.filter((p) => {
+									return p.id != player.id;
+								}, player);
+							}
+						}
+					}
+				})
+			},
+			'ArrowLeft': function (game) {
+				game.players.map((player) => {
+					if(player.id == socket.id && player.x > 0) {
+						player.x--;
+
+						for(let [index, coin] of game.coins.entries()) {
+							if(player.x == coin.x && player.y == coin.y) {
+								game.coins.splice(index, 1);
+								player.points++;
+							}
+						}
+						for(let [index, enemyPlayer] of game.players.entries()) {
+							if(player.x == enemyPlayer.x && player.y == enemyPlayer.y && player.id != enemyPlayer.id) {
+								game.players.splice(index, 1);
+								player.points = player.points + 10;
+							}
+						}
+						for(let [index, bomb] of game.bombs.entries()) {
+							if(player.x == bomb.x && player.y == bomb.y) {
+								game.bombs.splice(index, 1);
+								player.points = player.points - 10;
+								game.players = game.players.filter((p) => {
+									return p.id != player.id;
+								}, player);
+							}
+						}
+					}
+				})
+			},
+		}
+
+		if(key == "ArrowUp" || key == "ArrowDown" || key == "ArrowRight" || key == "ArrowLeft") {
+			keyHandler[key](game);
+		}
+
+		socket.broadcast.emit('renderGame', game);
+		socket.emit('renderGame', game);
+	});
+
 });
 
 
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,36 +1,44 @@
 const screen = document.getElementById('screen');
 const context = screen.getContext('2d');
+var playerList = document.getElementById('playerList');
 
-renderScreen = (game) => {
-    clearScreen();
+renderScreen = (game) => { 
+	clearScreen();
 
-    for(let playerId in game.players) {
-        const player = game.players[playerId]
-        context.fillStyle = player.color;
-        context.fillRect(player.x, player.y, 1, 1);
-    }
+	for(const coinId in game.coins) {
+		const coin = game.coins[coinId]
+		context.fillStyle = coin.color;
+		context.fillRect(coin.x, coin.y, 1, 1);
+	}
 
-    for(let coinId in game.coins) {
-        const coin = game.coins[coinId]
-        context.fillStyle = coin.color;
-        context.fillRect(coin.x, coin.y, 1, 1);
-    }
+	for(const bombId in game.bombs) {
+		const bomb = game.bombs[bombId]
+		context.fillStyle = bomb.color;
+		context.fillRect(bomb.x, bomb.y, 1, 1);
+	}
 
-    for(let bombId in game.bombs) {
-        const bomb = game.bombs[bombId]
-        context.fillStyle = bomb.color;
-        context.fillRect(bomb.x, bomb.y, 1, 1);
-    }
+	for(const playerId in game.players) {
+		const player = game.players[playerId]
+		context.fillStyle = player.color;
+		context.fillRect(player.x, player.y, 1, 1);
+	}
 
-    console.log(game);
 
-    socket.emit('renderGame');
 }
 
 clearScreen = () => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, 25, 25);
+	context.fillStyle = 'white';
+	context.clearRect(0, 0, 25, 25);
 }
+
+document.addEventListener('keydown', (event) => {
+	socket.emit('keyPress', event.key)
+});
+
+
+
+
+
 
 
 
